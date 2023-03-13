dfef087bb935bd26f947debdb44551f8348c2e45
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-03-13 18:48:28 -0300

    feat: add links to Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,54 @@
-import { House, Planet, Playlist } from 'phosphor-react';
+import { useState } from 'react';
+import { House, Planet, Playlist, CaretLeft, CaretRight } from 'phosphor-react';
+
+import { cn } from '../utils/classNames';
 
 import { SidebarLink } from './SidebarLink';
 
 export function Sidebar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <aside className="fixed top-0 left-0 h-full bg-slate-800/60 backdrop-blur drop-shadow-md w-64">
-      <div className="py-6">
-        <ul className="flex flex-col gap-2">
-          <SidebarLink icon={<House weight="bold" />} text="Home" />
-          <SidebarLink icon={<Planet weight="bold" />} text="Explore" />
-          <SidebarLink icon={<Playlist weight="bold" />} text="Genres" />
+    <aside
+      className={cn(
+        'py-2 fixed top-0 left-0 h-full bg-slate-800/60 backdrop-blur drop-shadow-md',
+        isSidebarOpen ? 'w-60' : 'w-fit'
+      )}
+    >
+      <button
+        type="button"
+        onClick={() => setIsSidebarOpen((prev) => !prev)}
+        aria-label={isSidebarOpen ? 'Close Menu' : 'Open Menu'}
+        aria-controls="main-navigation"
+        aria-expanded={isSidebarOpen}
+        className="absolute right-0 translate-x-1/2 p-2 bg-violet-500 rounded-full cursor-pointer hover:bg-violet-600 focus:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-slate-800 transition-colors duration-200"
+      >
+        {isSidebarOpen ? (
+          <CaretLeft weight="bold" className="h-6 w-6 text-slate-900" />
+        ) : (
+          <CaretRight weight="bold" className="h-6 w-6 text-slate-900" />
+        )}
+      </button>
+
+      <nav id="main-navigation" className="pt-16 transition-all duration-200">
+        <ul aria-label="main" className="flex flex-col gap-3">
+          <SidebarLink
+            icon={<House weight="bold" />}
+            text="Home"
+            isSidebarOpen={isSidebarOpen}
+          />
+          <SidebarLink
+            icon={<Planet weight="bold" />}
+            text="Explore"
+            isSidebarOpen={isSidebarOpen}
+          />
+          <SidebarLink
+            icon={<Playlist weight="bold" />}
+            text="Genres"
+            isSidebarOpen={isSidebarOpen}
+          />
         </ul>
-      </div>
+      </nav>
     </aside>
   );
 }
--- src/components/SidebarLink.tsx
+++ src/components/SidebarLink.tsx
@@ -5,9 +5,10 @@ import { NavLink } from './NavLink';
 interface SidebarLinkProps {
   icon: JSX.Element;
   text: string;
+  isSidebarOpen: boolean;
 }
 
-export function SidebarLink({ icon, text }: SidebarLinkProps) {
+export function SidebarLink({ icon, text, isSidebarOpen }: SidebarLinkProps) {
   const activeStyles =
     'before:absolute before:left-0 before:top-0 before:h-5 before:w-5 before:bg-teal-300 before:blur-xl';
 
@@ -17,23 +18,28 @@ export function SidebarLink({ icon, text }: SidebarLinkProps) {
     <NavLink.Root className={cn('py-2 relative', isActive ? activeStyles : '')}>
       <NavLink.Link
         href="/"
-        className="pl-6 flex items-center gap-4 group border-l-2 border-l-teal-500 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-slate-800"
+        className={cn(
+          'px-6 py-1 flex items-center gap-4 border-x-2 border-x-transparent group focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-slate-800',
+          isActive ? 'border-l-teal-500' : 'border-l-transparent'
+        )}
       >
         <NavLink.Icon
           icon={icon}
           className={cn(
-            'h-6 w-6 group-hover:text-teal-500 group-focus:text-teal-500',
+            'h-6 w-6 group-hover:text-teal-500 group-focus:text-teal-500 transition-colors duration-200',
             isActive ? 'text-teal-500' : 'text-gray-300'
           )}
         />
-        <NavLink.Text
-          className={cn(
-            'text-lg font-semibold group-hover:text-teal-500 group-focus:text-teal-500',
-            isActive ? 'text-teal-500' : 'text-gray-300'
-          )}
-        >
-          {text}
-        </NavLink.Text>
+        {isSidebarOpen ? (
+          <NavLink.Text
+            className={cn(
+              'text-lg font-semibold group-hover:text-teal-500 group-focus:text-teal-500 leading-none transition-colors duration-200',
+              isActive ? 'text-teal-500' : 'text-gray-300'
+            )}
+          >
+            {text}
+          </NavLink.Text>
+        ) : null}
       </NavLink.Link>
     </NavLink.Root>
   );

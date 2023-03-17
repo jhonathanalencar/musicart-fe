39510a77c01d37091c4d029cf159f87ac7e8dfae
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-03-17 16:10:25 -0300

    feat: add apiSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { apiSlice } from './api/apiSlice';
+
 export const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: import.meta.env.DEV,
 });
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { ThemeContextProvider } from './contexts/ThemeContext';
 
 import { App } from './App';
 
 import './index.css';
+import { store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeContextProvider>
   </React.StrictMode>
 );

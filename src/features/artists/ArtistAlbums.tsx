d54b74891efbb0e8980b0403eccc040bfbc2cf88
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-05-05 19:26:02 -0300

    feat: add skeleton to ArtistAlbums

--- src/features/artists/ArtistAlbums.tsx
+++ src/features/artists/ArtistAlbums.tsx
@@ -1,25 +1,41 @@
-import { Slider } from '../../components/Slider';
+import { useSlidesPerView } from '../../hooks/useSlidesPerView';
 import { useGetArtistAlbumsQuery } from './artistsApiSlice';
+
+import { Skeleton } from '../../components/Skeleton/Skeleton';
+import { SkeletonPlaylistCard } from '../../components/Skeleton/SkeletonPlaylistCard';
+import { Slider } from '../../components/Slider';
 
 interface ArtistAlbumsProps {
   artistId: string | undefined;
 }
 
 export function ArtistAlbums({ artistId }: ArtistAlbumsProps) {
-  const {
-    data: albums,
-    isLoading: isLoadingAlbums,
-    isError: isErrorAlbums,
-  } = useGetArtistAlbumsQuery(artistId);
+  const { data, isLoading, isError } = useGetArtistAlbumsQuery(artistId);
 
-  if (!albums) return null;
+  const { slidesPerView } = useSlidesPerView();
+
+  if (isLoading) {
+    return (
+      <div className="w-full flex flex-col gap-3 mt-4">
+        <Skeleton classes="title width-50" />
+
+        <div className="w-full flex gap-2">
+          {[...Array(slidesPerView).keys()].map((item) => {
+            return <SkeletonPlaylistCard key={item} />;
+          })}
+        </div>
+      </div>
+    );
+  }
+
+  if (!data || isError) return null;
 
   return (
     <div className="w-full flex flex-col gap-3 mt-4">
       <h2 className="text-lg md:text-3xl font-semibold text-gray-700 dark:text-gray-200">
         Albums
       </h2>
-      <Slider sliderItems={albums} />
+      <Slider sliderItems={data} />
     </div>
   );
 }
